0cff69c3e2d235aaf395cd86fdb0611b80a1c120
xiaohe55 <1185462886@qq.com>
2018-03-29 20:43:41 +0800

    note

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # xiaoxiangxueyuan
 小象学院基础课程
+day0329:
+1. True False 大写
+2.input 默认为string类型
---
+++ day0329/demo1_ variable.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+name = "zhangsan"
+weight = 82
+age = 20
+print(name)
+weight=weight+5
+print(weight)
+
+print(type(name))
+print(type(age))
