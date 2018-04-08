9275df2589ff36492dbf24726146dd1a969e1a5f
xiaohe55 <1185462886@qq.com>
2018-04-08 20:26:10 +0800

    list add

--- day0403/demo_list.py
+++ day0403/demo_list.py
@@ -7,10 +7,13 @@
 # name_list[0] = "xiaobai"
 # print(name_list[0])
 
-#info_list = ["zhangsan", 20, 180.5]
-#print(info_list[2])
-#i = 0
-#while遍历
+
+# 存储不同类型
+# info_list = ["zhangsan", 20, 180.5]
+# 下标不可越界
+# print(info_list[2])
+# i = 0
+# while遍历
 # while i < len(info_list):
 #     print(info_list[i])
 #     i+=1
@@ -24,10 +27,12 @@
 #     print(item)
 
 
-infos_list = [ ["zhangsan", 20, 180.5], ["lisi", 21, 190.5], ["wangwu", 22, 195.5]]
-#print(infos_list[0][0])
-#print(infos_list[1][0])
+# 嵌套列表
+infos_list = [["zhangsan", 20, 180.5], ["lisi", 21, 190.5], ["wangwu", 22, 195.5]]
+# print(infos_list[0][0])
+# print(infos_list[1][0])
 for lst in infos_list:
     print(lst)
     for item in lst:
         print(item)
+
